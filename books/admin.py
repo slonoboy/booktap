54289765ffa50f053a29f8a3d8c6b61e0bdcd0cf
slonoboy <52863393+slonoboy@users.noreply.github.com>
2022-03-27 11:12:25 +0600

    asdf

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from books import models
 
 # Register your models here.
+admin.site.register(models.Account)
+admin.site.register(models.Request)
+admin.site.register(models.Review)
+admin.site.register(models.Library)
+admin.site.register(models.Book)
+admin.site.register(models.BookOwned)
+admin.site.register(models.Borrow)
+admin.site.register(models.Transaction)
+admin.site.register(models.SocialRating)
