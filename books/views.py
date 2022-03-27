b95bcc322f8e1ef49d0e6dd8d585801315b94f41
Bekarys <magauiyanov.b@gmail.com>
2022-03-27 06:34:51 +0600

    HAZBIK

--- books/views.py
+++ books/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login ,logout
+from django.shortcuts import redirect, render
+from .models import*
+from django.contrib.auth.models import User
 
 # Create your views here.
 def profil_unik(request):
@@ -14,13 +17,24 @@ def profile(request):
     return render(request,'books/profile.html')
 
 def signin(request):
+    if request.POST.get("signin"):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            if request.user.is_superuser == True:
+                return redirect('moderator/')
+            else:
+                return redirect('main/')
+        
     return render(request,'books/signin.html')
 
 def signup(request):
-    if request.POST.get("signin"):
-        print(request.POST.get("username"))
-        print(request.POST.get("password"))
-        
+    if request.POST.get("signup"):
+
+        acc = Account.objects.create(email=request.POST.get("email"),password=request.POST.get("password"),first_name=request.POST.get("name"),last_name=request.POST.get("last_name"),phone_number=request.POST.get("phonw"),id_number=request.POST.get("id_num"),library_id=Library(id=1),date_joined='2021-03-27',last_login='2021-03-27',is_admin=False,is_active=True,is_librarian=False,is_premium=True,is_staff=False,is_superuser=False)
+        user = User.objects.create(username=request.POST.get("id_num"),first_name=request.POST.get("name"),last_name=request.POST.get("last_name"),password=request.POST.get("password"))
     return render(request,'books/signup.html')
 
 def books(request):
